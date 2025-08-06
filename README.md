5aacbd30a808b609c0f7c7feca3b1f8d91ba6869
Niharika-Seth <niharikaseth1112@gmail.com>
2025-08-06 18:48:44 +0530

    fiveaug

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Python
 Python Programming Language 
+Interview Questions
+
---
+++ Variables and datatypes.py
@@ -0,0 +1,23 @@
+# VARIABLES AND DATATYPES
+
+# How to display Output
+print ("Hello world")
+print("I LIKE TO CODE IN PYTHON PROGRAMMING LANGUAGE")
+print(23)
+
+# ARITHMETIC OPERATORS AND VARIABLES
+a = 50
+b = 10
+print("Sum of two numbers are: ", (a+b))
+print("Difference of two numbers are:" , (a-b))
+print("Product of two numbers are: " , (a*b))
+print("Quotient of two numbers are" , (a/b))
+print("Remainder of two numbers are: ",(a%b))
+
+# DATA TYPES AND VARIABLES
+name = "Niharika Seth"
+print("My name is", name)
+age = 23
+print("My age is" , age)
+price = 28.59
+print("Price of this pen is",price)
