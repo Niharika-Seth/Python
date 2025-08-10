6c6a80f10640e3a39b2bf5becb24360a515b3e0a
Niharika-Seth <niharikaseth1112@gmail.com>
2025-08-10 09:20:01 +0530

    Added strings and conditional statements file

---
+++ stringsandconditionalstatements.py
@@ -0,0 +1,19 @@
+# How to define a string
+str1 = "This is a string. "
+str2 = 'This is a string.'
+str3 = """This is a string."""
+print(str1)
+print(str2)
+print(str3)
+
+# Why we use double quotes and triple quotes?---> In order to use single quotes inside it.
+str = "My name is 'Niharika Seth'."
+print(str)
+
+# Escape Sequence Characters ----> \n , \t
+ab = "My domain is AI & DS.\nI like coding in python and learning different algorithms."
+print(ab)
+
+bc = "My domain is AI & DS.\t\tI like coding in python and learning different algorithms."
+print(bc)
+
