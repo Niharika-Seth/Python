845dfe8e49bd78e0afe3ec94cc59dd06b8516021
Niharika-Seth <niharikaseth1112@gmail.com>
2025-08-11 11:15:51 +0530

    Added Dictionary program file

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+# How to define dictionary 
+info = {
+  "Name" : "Niharika",
+  "Age" : 23,
+  "Domain" : "AI & DS"
+}
+print(info)
+
+# We can also store list or tuples in a dictionary (Key can only accept int,float,string, tuple data type and not list.)
+dict = {
+  "Name" : "Niharika",
+  "Subjects" : ["Maths" , "Science" , "English" ,"Civil"],
+  "Marks" : (50,85,99,79)
+}
+print(dict)
+print("the data type of dict is: " , type(dict))
+
