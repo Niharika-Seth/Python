0fe32c5a072fa4b7060b19c1d763b6af81667096
Niharika-Seth <niharikaseth1112@gmail.com>
2025-08-11 09:44:08 +0530

    Added Tuple program file

---
+++ tuple.py
@@ -0,0 +1,14 @@
+# How to define tuples 
+t = (1,2,3,4,'python')
+print(t)
+print("the data type of t is: " , type(t))
+
+# Tuples are immutable built in data type that stores values of different data types together.
+# To create a tuple with single value always add , at the end of the element. (with multiple values , is optional)
+a = (1,)
+print("The data type of a is: " , type(a))
+
+# To access elements of tuple
+print("The element at 1 index: " , t[1])
+print("The element at 4 index: " , t[4])
+
