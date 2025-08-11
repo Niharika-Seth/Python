36d41be58b037bf5ba7a13f8728769db694222ee
Niharika-Seth <niharikaseth1112@gmail.com>
2025-08-11 11:53:21 +0530

    Added content of Nested Dictionary And Dictionary Methods

--- dictionary.py
+++ dictionary.py
@@ -29,3 +29,42 @@ print(dict)
 # To create empty dictionary
 null_dict = {}
 print("The data type of null_dict: " , type(null_dict)) 
+
+# Nested Dictionary
+stud = {
+  "Name" : "Niha",
+  "Subjects" : {
+    "phy" : 55,
+    "chem" : 67,
+    "bio" : 90,
+    "comps" : 99
+  },
+  "age" : 19
+}
+print(stud)
+print(stud["Subjects"])
+print(stud["Subjects"] ["comps"])
+
+# Dictionary Methods
+# keys() function ---> return all the keys of dictionary
+print(dict.keys())
+
+# To get total number of keys we use length function
+print("Total number of keys in a dictionary : " , len(dict))
+
+# values() function ----> return all the value of dictionary
+print(dict.values())
+
+# items() ----> returns all the key:value pairs in form of tuples
+print(dict.items())
+
+# get("key") function ----> returns the key according to the value. (.get() does not throw error)
+print(dict.get("Subjects"))
+print(dict.get("Marks"))
+
+# update({new_dict or key:value}) ----> insert the specified items to the dictionary
+dict.update({"City" : "Mumbai"})
+print(dict)
+new_dict = {"Phone number" : 900468296, "DOB" : "22 July 2004"}
+dict.update(new_dict)
+print(dict)
