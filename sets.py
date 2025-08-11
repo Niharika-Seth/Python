d443eacadd422f5411cb4a7c742b7e9132a87bcf
Niharika-Seth <niharikaseth1112@gmail.com>
2025-08-11 12:07:46 +0530

    Added Sets program file

---
+++ sets.py
@@ -0,0 +1,11 @@
+# How to define sets
+s = {1,2,3,4,5,"python","java",4}
+print(s)
+print("The data type of set is : " ,type(s))
+
+# set ignores duplicate items
+print("The length of set: " , len(s))
+
+# To create empty sets
+sets = set()
+print(type(sets))
