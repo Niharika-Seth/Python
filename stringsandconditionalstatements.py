f51cde2b9515b5b263cbf7250f051d52e86102d3
Niharika-Seth <niharikaseth1112@gmail.com>
2025-08-11 07:52:49 +0530

    Added Content related to Types of String Functions

--- stringsandconditionalstatements.py
+++ stringsandconditionalstatements.py
@@ -54,3 +54,39 @@ print(n4)
 print(n5)  
 print(n6)
 print(n7)
+
+# String Functions
+# endswith() function ----> to check if the string ends with given substring
+strc = "computer vision"
+print("Is the string ending with on? : " , strc.endswith("on"))
+print("Is the string ending with eh? : " , strc.endswith("eh"))
+
+# capatalize() function ----> capitalize the 1st character of a string
+print(strc.capitalize())
+
+# replace() function ----> to replace old substring with new substring
+strd = "I like python programming language"
+print("Replacing i with w :  " , strd.replace("i" , "w"))
+print("Replacing python with java: " , strd.replace("Python" , "Java"))
+
+# find() function ----> to search for the word in a string and if the word exist it returns the index of its first occurence
+print("Python word in the string : " , strd.find("Python"))
+print("i in the string: " , strd.find("i"))
+print("I in the string: " , strd.find("I"))
+print("Letter q in a string: " , strd.find("q"))   # returns -1 as it doesnt exist in the string , negative index is only valid for slicing 
+
+# count() Function
+print("The letter 'a' in a string: " , strd.count("a"))
+print("The word python in a string : " , strd.count("python"))
+
+# WAP to input user's first name and print it's length
+a = input("Enter your first name: ")
+print("The length of your first name is : " , len(a))
+
+# WAP to find the occurences of '$' in a string
+b = "hi , $i am the $ symbol , $my name is $99.69"
+print("The occurence of $ in a string is: " , b.count("$"))
+
+
+
+
